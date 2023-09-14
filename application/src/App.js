ce39433e5f71654de41f47840710f38d3542702a
ali32901 <80500778+ali32901@users.noreply.github.com>
2023-09-13 20:45:38 -0700

    delete commented code in App.js

--- application/src/App.js
+++ application/src/App.js
@@ -4,25 +4,23 @@ import {
     Routes,
     Route,
 } from 'react-router-dom';
-// import About from './pages/About';
-// import MemberAndy from './pages/MemberAndy';
+
 import AppRoutes from './AppRoutes';
 
 const App = () => {
 
-    return(
+    return (
         <>
             <BrowserRouter>
                 <Routes>
                     {AppRoutes.map((route, idx) => {
-                        const { element, ...rest} = route;
+                        const { element, ...rest } = route;
                         return <Route key={idx} {...rest} element={element} />;
                     })}
-                    {/* <Route path='/' element={<About />} />
-                    <Route path='/MemberAndy' element={<MemberAndy />} /> */}
+
                 </Routes>
             </BrowserRouter>
-             
+
         </>
     );
 }
