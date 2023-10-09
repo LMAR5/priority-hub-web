9f0afc1ae1f45599697bb4f87e5bb9d0e758d3e4
Tyler Tam <tytam1326@gmail.com>
2023-10-09 10:24:36 -0700

    Adding json data to table

--- application/frontend/src/components/Tasks/Tasks.js
+++ application/frontend/src/components/Tasks/Tasks.js
@@ -1,32 +1,40 @@
 import React from 'react';
 import Table from 'react-bootstrap/Table';
+import jsonData from './TasksJSONSample.json'
 import getAllTasks from '../../services/TaskService'
 
 const Tasks = () => {
+    const data = jsonData.map(
+        (info) => {
+            return(
+                <tr>
+                    <td>{info.title}</td>;
+                    <td>{info.category}</td>;
+                    <td>{info.status}</td>;
+                    <td>{info.priority}</td>;
+                    <td>{info.duedate}</td>;
+                    <td>{info.lastupdateddatetime}</td>;
+                </tr>
+            )
+        }
+    )
     return(
         <div>
             <h1>Test</h1>
         <Table striped bordered hover>
             <thead>
             <tr>
-                  <th>Title</th>
-                  <th>Category</th>
-                  <th>Status</th>
-                  <th>Priority</th>
-                  <th>DueDate</th>
-                  <th>Last modification.</th>
+                  <th>Title</th>;
+                  <th>Category</th>;
+                  <th>Status</th>;
+                  <th>Priority</th>;
+                  <th>DueDate</th>;
+                  <th>Last modification.</th>;
                 </tr>
             </thead>
 
             <tbody>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
+                {data};
             </tbody>
         </Table>
         </div>
