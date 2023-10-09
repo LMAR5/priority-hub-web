434fb2e6a891a4d066bfe249763da56893680672
ali32901 <80500778+ali32901@users.noreply.github.com>
2023-10-09 13:24:28 -0700

    Duplicate search from task to category

--- application/frontend/src/components/Categories/Categories.js
+++ application/frontend/src/components/Categories/Categories.js
@@ -1,15 +1,67 @@
-import React from 'react';
-import Table from 'react-bootstrap/Table';
+import React, {useState} from 'react';
 import CategoriesTable from './CategoriesTable';
+import CategoryService from '../../services/CategoryService'
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/esm/Row';
+import Col from 'react-bootstrap/esm/Col';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 const Categories = () => {
-  return (
-    <>
-      <h1>Categories</h1>
-      <CategoriesTable />
-    </>
 
-  )
+  const [loadingCategoryTable, setLoadingCategoryTable] = useState(false);
+  const [loadingSearchBar, setLoadingSearchBar] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+
+
+  const renderCategoryTable = () => {
+    
+      return(
+        <CategoriesTable /> 
+      )
+  }
+
+  const updateSearchTermBar = (event) => {
+    //console.log(event.target.value);
+    setSearchTerm(event.target.value);
+  }
+
+  const performSearch = () => {
+    CategoryService.searchCategory().then((data) => {
+        console.log("Search Category response:",data);
+        //Update variable that stores the list of categories from backend.
+    });
+  }
+
+  const renderSearchBar = () => {
+    return(
+        <InputGroup className="mb-1 mt-3">
+            <Form.Control
+                placeholder="Search..."
+                value={searchTerm}
+                onChange={(event) => {updateSearchTermBar(event)}} />
+            <Button variant="primary" onClick={() => {performSearch()}}>Search</Button>
+        </InputGroup>
+    );
+  }
+
+  let contentTable = loadingCategoryTable ? <h2>Loading...</h2> : renderCategoryTable();
+  let showSearchBard = loadingSearchBar ? <h2>Loading...</h2> : renderSearchBar();
+
+  return(
+    <div>
+        <Row>
+            <Col md={{ span: 4, offset: 8 }}>{showSearchBard}</Col>
+        </Row>
+        <Row className="mt-3">
+        <Col>
+            <h1>Categories</h1>
+            {contentTable}
+        </Col>
+    </Row>
+    </div>                
+    );
+
 };
 
 export default Categories;
