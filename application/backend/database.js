77d761db188fce40fdad8f46b37f3e9b2104a4cf
Luis Aguilar <luisaguilar159@gmail.com>
2023-10-10 02:10:37 -0700

    Updating port for DB connection

--- application/backend/database.js
+++ application/backend/database.js
@@ -6,5 +6,6 @@ module.exports = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSW,
-    database: process.env.DB_SCHEMA
+    database: process.env.DB_SCHEMA,
+    port: process.env.DB_PORT
 });
