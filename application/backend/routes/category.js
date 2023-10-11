4cc9cf7f4abf229fb23b1bb8902b9701b8bb110a
LamTran2530 <ctw2539@gmail.com>
2023-10-10 23:22:00 -0700

    SearchCategory backend completed

--- application/backend/routes/category.js
+++ application/backend/routes/category.js
@@ -1,4 +1,4 @@
-const { Router } = require('express');
+const { Router, request } = require('express');
 //Import MySQL connection
 const db = require('../database');
 
@@ -12,10 +12,20 @@ router.use((request, response, next) => {
 // Uri: http://localhost:3001/CategoryController/GetAllCategories
 // Type: GET
 // Description: Get list of all the categories in DB
+router.get('/GetAllCategories', async (request, response) => {
+    const results = await db.promise().query(`SELECT * FROM Category`);
+    response.status(200).send(results[0]);
+});
 
 
 // Uri: http://localhost:3001/CategoryController/SearchCategory
 // Type: GET
 // Description: Search a category by its name or other fields sending a keyword from frontend
+router.get('/SearchCategory', async (request, response) => {
+    const keyTerm = request.query.key;
+    console.log("keyTerm:",keyTerm);
+    const results = await db.promise().query(`SELECT * FROM Category WHERE Title like '%${keyTerm}%' OR Description LIKE '%${keyTerm}%'`);
+    response.status(200).send(results[0]);
+});
 
 module.exports = router;
