05e5e7ee16d35455f78039aad9ffbddf458a7a23
ali32901 <80500778+ali32901@users.noreply.github.com>
2023-10-17 16:55:43 -0700

    Add fake description to landing page

--- application/frontend/src/components/LandingPage/LandingPageMain.js
+++ application/frontend/src/components/LandingPage/LandingPageMain.js
@@ -2,28 +2,38 @@ import React from 'react';
 import Row from 'react-bootstrap/esm/Row';
 import Col from 'react-bootstrap/esm/Col';
 import Container from 'react-bootstrap/esm/Container';
+import Image from 'react-bootstrap/Image';
+
+import '../../assets/css/LandingPage.css'
 
 function LandingPageMain () {
     return(
-        <div>
-            <Row>
-                <Container className='p-4 mt-3 bg-light rounded-3'>
-                    <h1 className='header'>Priority Hub Landing page</h1>
-                </Container>
-            </Row>
-            <Row className='mt-4' style={{ height: '150px' }}>
+        <Container>
+            <Row className = "mt-4">
+                <Col>Picture</Col>
                 <Col>
-                    <h2>Feature 1</h2>
-                    <p>Description of feature and a picture maybe</p>
+                    <h2>Organize Your Work</h2>
+                    <p2>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p2>
                 </Col>
             </Row>
-            <Row className='mt-4' style={{ height: '150px' }}>
+            <Row className = "mt-4 bg-light" >
                 <Col>
-                    <h2>Feature 2</h2>
-                    <p>Description of feature and a picture maybe</p>
+                <h2>See How You Did</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
                 </Col>
+                <Col>Picture</Col>
             </Row>
-        </div>        
+        </Container>      
     );
 }
 
