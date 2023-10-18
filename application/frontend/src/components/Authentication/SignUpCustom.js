d0851aef9b20353d53fc726645f3624b44393923
ali32901 <80500778+ali32901@users.noreply.github.com>
2023-10-18 15:34:46 -0700

    Add input and button for signup

--- application/frontend/src/components/Authentication/SignUpCustom.js
+++ application/frontend/src/components/Authentication/SignUpCustom.js
@@ -1,8 +1,43 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-function SignUpCustom(){
-    return(
-        <h1>Sign Up custom</h1>
+function SignUpCustom() {
+    return (
+        <>
+            <h1>Sign Up custom</h1>
+
+            <Container>
+                <Row>
+                    <Col>
+                        <Form>
+                            <Form.Group className='mb-3' controlId='formUsername' />
+                            <Form.Control type='user' placeholder='Enter username' />
+
+                            <Form.Group classname='mb-3' controlId='formBasicEmail' />
+                            <Form.Control type='email' placeholder='Enter email' />
+
+
+                            <Form.Group classname='mb-3' controlId='formPassword' />
+                            <Form.Control type='password' placeholder='Enter password' />
+
+                            <Form.Group classname='mb-3' controlId='formConfirmPassword' />
+                            <Form.Control type='password' placeholder='Confirm password' />
+
+                            <Button variant="primary">Sign Up</Button>{' '}
+                        </Form>
+                    </Col>
+                </Row>
+
+
+
+
+            </Container>
+
+        </>
     );
 }
 
