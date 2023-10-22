32dfad2d8b5e57d6e2c533f890d1bf77107fa739
ali32901 <80500778+ali32901@users.noreply.github.com>
2023-10-21 20:20:42 -0700

    Create HTTP post request for Sign Up

--- application/frontend/src/components/Authentication/SignUpCustom.js
+++ application/frontend/src/components/Authentication/SignUpCustom.js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import '../../assets/css/SignUp.css'
 
 function SignUpCustom() {
+
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+
+    const HandleSubmit = (e) => {
+        e.preventDefault();
+        const user = { username, email, password, };
+
+        fetch('http://localhost:3001/api/AuthController/SignUp', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(user)
+        }).then(() => {
+
+        })
+    }
+
     return (
         <>
             <div className='d-flex justify-content-center'>
-                <Form className='signup rounded'>
+                <Form className='signup rounded' onSubmit={HandleSubmit}>
                     <h2 className='mb-3'>Sign Up</h2>
                     <Form.Group className='mb-3' controlId='formName'>
-                        <Form.Control type='user' placeholder='Enter name' />
+                        <Form.Control type='' placeholder='Enter name' onChange={(e) => setUsername(e.target.value)} />
                     </Form.Group>
 
                     <Form.Group className='mb-3' controlId='formBasicEmail' >
-                        <Form.Control type='email' placeholder='Enter email' />
+                        <Form.Control type='email' placeholder='Enter email' onChange={(e) => setEmail(e.target.value)} />
                     </Form.Group>
 
                     <Form.Group className='mb-3' controlId='formPassword' >
-                        <Form.Control type='password' placeholder='Enter password' />
+                        <Form.Control type='password' placeholder='Enter password' onChange={(e) => setPassword(e.target.value)} />
                     </Form.Group>
 
                     <Form.Group className='mb-3' controlId='formConfirmPassword' >
@@ -33,3 +53,5 @@ function SignUpCustom() {
 }
 
 export default SignUpCustom;
+
+
