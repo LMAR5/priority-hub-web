2cf77f95681fea1ac0815a7e3198d6b41178c697
Luis Aguilar <luisaguilar159@gmail.com>
2023-10-28 19:28:11 -0700

    PH-37 | Add backend methods for signin
    Add backend method to validate credentials against DB and generate session token.

--- application/backend/routes/authentication.js
+++ application/backend/routes/authentication.js
@@ -12,6 +12,25 @@ router.use((request, response, next) => {
 // Uri: http://localhost:3001/api/AuthController/SignIn
 // Type: [pending]
 // Description: Method that checks credentials provided by user (input) against DB
+router.post('/SignIn', (request, response) => {
+    const emailSignIn = request.body.email;
+    const passwordSignIn = request.body.password;
+
+    const queryDB = 'SELECT * FROM User WHERE Email=? AND Password=?';
+    const values = [emailSignIn, passwordSignIn];
+
+    db.query(queryDB, values, (err, result) => {
+        if (err) {
+            return response.status(500).json({ message: 'Duplicate email entry in database', result: [], success: false });
+        }
+
+        if (result.length > 0) {
+            return response.status(201).json({ message: 'Access granted', result: result[0],  success: true,  token: 'priorityhubtoken123456789' });
+        } else {
+            return response.status(500).json({ message: 'Invalid credentials', result: [], success: false });
+        }
+    });
+});
 
 // Uri: http://localhost:3001/api/AuthController/SignUp
 // Type: Post
@@ -19,24 +38,24 @@ router.use((request, response, next) => {
 router.post('/SignUp', (request, response, next) => {
     // console.log(request.body);
     const { firstname, lastname, email, password } = request.body;
-  
+
     if (!firstname || !lastname || !email || !password) {
-        return response.status(400).json({ error: 'Missing required data' });
+        return response.status(400).json({ message: 'Missing required data', result: false });
     }
-  
-    const insertQuery = 'INSERT INTO User (firstname, lastname, email, password, Status) VALUES (?, ?, ?, ?, ?)';
-    const values = [firstname, lastname, email, password, 'ACTIVE'];
-  
+
+    const insertQuery = 'INSERT INTO User (firstname, lastname, email, password, Status, createdby, lastupdatedby) VALUES (?, ?, ?, ?, ?, ?, ?)';
+    const values = [firstname, lastname, email, password, 'ACTIVE','System','System'];
+
     db.query(insertQuery, values, (err, result) => {
         if (err) {
             console.error('Error in SignUp route:', err.code);
-            return response.status(500).json({ message: 'Duplicate email entry in database' });
+            return response.status(500).json({ message: 'Duplicate email entry in database', result: false });
         }
 
         if (result.affectedRows === 1) {
-            return response.status(201).json({ message: 'User registered successfully' });
+            return response.status(201).json({ message: 'User registered successfully', result: true });
         } else {
-            return response.status(500).json({ message: 'Failed to register user' });
+            return response.status(500).json({ message: 'Failed to register user', result: false });
         }
     });
 });
